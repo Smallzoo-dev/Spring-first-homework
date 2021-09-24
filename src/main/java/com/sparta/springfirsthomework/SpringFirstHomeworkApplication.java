ef49848486eafc27d959f876b37ec3f6c37aa1eb
Marlcocoro <joongyue@gmail.com>
2021-09-24 20:10:27 +0900

    first commit

---
+++ src/main/java/com/sparta/springfirsthomework/SpringFirstHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springfirsthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFirstHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springfirsthomework/SpringFirstHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springfirsthomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
